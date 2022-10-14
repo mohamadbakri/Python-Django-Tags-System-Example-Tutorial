ee595d402560e93fc7fd11128bfbf25d9cd01645
mohamadbakri <mohammed_bakri@hotmail.com>
2022-10-14 14:27:46 +0200

    update templats to templates and modifiy url.py

--- example/core/urls.py
+++ example/core/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('tag-system/', views.home_view, name="home"),
     path('post/<slug:slug>/', views.detail_view, name="detail"),
     path('tag/<slug:slug>/', views.tagged, name="tagged"),
+
 ]
